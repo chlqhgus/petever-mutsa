97d6ae0a83abfdd60eb2a2d5a7ba1a516756cea3
yoominwoo <mwlucas@naver.com>
2024-08-02 01:23:35 +0900

    [FIX] App.jsx 내 불필요한 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,6 @@ import ModalComplete from "./components/common/ModalComplete";
 function App() {
   return (
     <>
-      <Button
-        color={ButtonSecondary}
-        text={"기록하기"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      ></Button>
-      <PostBlock
-        img={img1}
-        textMain={"보고싶은나의햄쥐돌아와줘"}
-        textSub={"김멋사"}
-        onClick={() => {
-          console.log("게시물 클릭");
-        }}
-      ></PostBlock>
-      <ModalComplete
-        onCloseClick={() => {
-          console.log("닫기 버튼 클릭");
-        }}
-      ></ModalComplete>
       <Router />
     </>
   );
